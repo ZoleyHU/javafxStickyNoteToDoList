efb4667795bed1d5baadf6a7bc0b6e0fe6666050
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2024-04-17 18:11:26 +0200

    New: added error message and check for empty note description. Removed unused import

--- src/main/java/hu/sticky/controller/ModifyNoteController.java
+++ src/main/java/hu/sticky/controller/ModifyNoteController.java
@@ -4,13 +4,13 @@ import hu.sticky.dao.NoteDaoImpl;
 import hu.sticky.model.StickyNote;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.time.LocalDate;
 
 public class ModifyNoteController {
@@ -20,14 +20,16 @@ public class ModifyNoteController {
 
     public void modifyNote(ActionEvent actionEvent) {
         StickyNote stickyNote = getData();
-        if (new NoteDaoImpl().modify(stickyNote)) {
-            //todo check if binded properties work, so events become unnecessary
-            //todo emit event, to update the notes on screen
+        if (isDescriptionNotEmpty(stickyNote.getNoteDescription())) {
+            if (new NoteDaoImpl().modify(stickyNote)) {
+                //todo check if binded properties work, so events become unnecessary
+                //todo emit event, to update the notes on screen
 
-            closeWindow(actionEvent);
-        } else {
-
+                closeWindow(actionEvent);
+                return;
+            }
         }
+        showError();
     }
 
     public void closeModifyNoteForm(ActionEvent actionEvent) {
@@ -38,6 +40,18 @@ public class ModifyNoteController {
         Node source = (Node) actionEvent.getSource();
         Stage stage = (Stage) source.getScene().getWindow();
         stage.close();
+    }
+
+    private void showError() {
+        Alert failedAlert = new Alert(Alert.AlertType.ERROR);
+        failedAlert.setHeaderText("Unable to modify note!");
+        failedAlert.setContentText("Please try again later and ensure you are connected to the database");
+
+        failedAlert.show();
+    }
+
+    private boolean isDescriptionNotEmpty(String description) {
+        return !description.trim().isEmpty();
     }
 
     public void setData(StickyNote stickyNote) {
