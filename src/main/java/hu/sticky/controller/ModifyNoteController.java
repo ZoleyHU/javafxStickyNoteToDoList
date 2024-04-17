fdcf8df4577e8d469a8f1dbb0618908be14800ef
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2024-04-17 14:00:55 +0200

    New: creating modifyNote.fxml and its controller

---
+++ src/main/java/hu/sticky/controller/ModifyNoteController.java
@@ -0,0 +1,18 @@
+package hu.sticky.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+
+public class ModifyNoteController {
+    public TextArea descriptionTextArea;
+    public DatePicker deadlineDate;
+    public ColorPicker noteColor;
+
+    public void modifyNote(ActionEvent actionEvent) {
+    }
+
+    public void closeModifyNoteForm(ActionEvent actionEvent) {
+    }
+}
